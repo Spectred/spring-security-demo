78266e02f5a8d7848836fa75b81ffaec876d303b
spectred <1505073336@qq.com>
2023-06-05 22:03:39 +0800

    :art: add base code

---
+++ spring-security-alpha/src/main/java/pers/spectred/security/alpha/SpringSecurityAlphaApplication.java
@@ -0,0 +1,13 @@
+package pers.spectred.security.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAlphaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAlphaApplication.class, args);
+    }
+
+}
---
+++ spring-security-alpha/src/test/java/pers/spectred/security/alpha/SpringSecurityAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.spectred.security.alpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAlphaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
