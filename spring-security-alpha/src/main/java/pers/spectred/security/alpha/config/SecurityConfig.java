4b1665a39af99f81c9c50a3bac25b1518c7ea042
spectred <1505073336@qq.com>
2023-06-05 22:42:06 +0800

    :art: Config add formLogin (to fix problem)

--- spring-security-alpha/src/main/java/pers/spectred/security/alpha/config/SecurityConfig.java
+++ spring-security-alpha/src/main/java/pers/spectred/security/alpha/config/SecurityConfig.java
@@ -15,10 +15,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+
                 // 开启Basic认证
-                .httpBasic(Customizer.withDefaults())
+                // .httpBasic(Customizer.withDefaults())
+                // 开启表单认证
+                .formLogin(formLogin -> {
+                    formLogin.loginPage("/login.html").loginProcessingUrl("/login");
+                })
                 // 所有请求都需要认证
-                .authorizeHttpRequests(authorizeRequest -> authorizeRequest.anyRequest().authenticated());
+                // .authorizeHttpRequests(authorizeHttpRequest -> authorizeHttpRequest.anyRequest().authenticated())
+                // 放行登录页面
+                .authorizeHttpRequests(authorizeHttpRequest -> authorizeHttpRequest.requestMatchers("/login.html").permitAll());
         return http.build();
     }
 }
